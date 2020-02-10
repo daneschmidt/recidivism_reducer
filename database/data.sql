b164741da704af8785ddc85411a863adb8f8ffb2
Travis Johnston <travisjohnston4588@icloud.com>
2020-02-10 11:26:05 -0600

    added test data for DB

---
+++ database/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO "address"
+    ("id", "clients_id", "street", "city", "state", "zip", "business", "home", "current")
+VALUES
+    ('1', '1', '215th Court', 'Peculiar', '64078', 'Burple Interprises', 'true', 'true');
+
+INSERT INTO "clients_competitions"
+    ("id", "clients_id", "competitions_id", "statusNum", "award", "notes")
+VALUES
+    ('1', '1', '1', '1', 'Congrats', 'You are awewsome!');
