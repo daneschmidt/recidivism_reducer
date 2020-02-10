a37ff7c04295612249dde87c50ed9dda577c44ee
Travis Johnston <travisjohnston4588@icloud.com>
2020-02-10 12:32:13 -0600

    restructured db.sql

--- database/data.sql
+++ database/data.sql
@@ -1,12 +1,3 @@
-INSERT INTO "address"
-    ("id", "clients_id", "street", "city", "state", "zip", "business", "home", "current")
-VALUES
-    ('1', '1', '215th Court', 'Peculiar', '64078', 'Burple Interprises', 'true', 'true');
-
-INSERT INTO "clients_competitions"
-    ("id", "clients_id", "competitions_id", "statusNum", "award", "notes")
-VALUES
-    ('1', '1', '1', '1', 'Congrats', 'You are awewsome!');
 INSERT INTO "user"
     ("firstName", "lastName", "password", "securityLevel", "phoneNumber", "email", "role", "isActive")
 VALUES
@@ -15,13 +6,23 @@ VALUES
 INSERT INTO "clients"
     ("firstName", "lastName", "gender", "phoneNumber", "email", "incarcerationLength", "releaseDate", "docNumber", "stateIncarcerated", "paroleOnRelease", "connections", "ifYesConnections", "business", "profilePic", "timeStamp", "isActive")
 VALUES
-    ('Dane', 'Pherigo', 'Male', 8162345678, 'd.pherigo@gmail.com', '2 years', 2020-02-10 , '22', 'Missouri', True, True, 'Yes I do', 'Dane Pherigo Coffee Shop', 'JoshBW.png', 2020-02-10
-04:05:06, True);
+    ('Dane', 'Pherigo', 'Male', 8162345678, 'd.pherigo@gmail.com', '2 years', 2020-02-10 , '22', 'Missouri', True, True, 'Yes I do', 'Dane Pherigo Coffee Shop', 'JoshBW.png', '2020-02-10
+04:05:06', True);
+
+INSERT INTO "address"
+    ("id", "clients_id", "street", "city", "state", "zip", "business", "home", "current")
+VALUES
+    ('1', '1', '215th Court', 'Peculiar', '64078', 'Burple Interprises', 'true', 'true');
 
 INSERT INTO "competitions"
     ("date of", "name")
 VALUES
     ('2020-03-10', 'Dane Pherigo');
+
+INSERT INTO "clients_competitions"
+    ("id", "clients_id", "competitions_id", "statusNum", "award", "notes")
+VALUES
+    ('1', '1', '1', '1', 'Congrats', 'You are awewsome!');
 
 INSERT INTO "links"
     ("clients_id", "url")
@@ -31,20 +32,20 @@ VALUES
 INSERT INTO "tasks"
     ("users_id", "clients_id", "task", "complete", "assignedOn", "dueBy")
 VALUES
-    ( 1, 1, 'Make sure Dane Pherigo has his LLC', False,  , 2020-02-11
-04:05:06);
+    ( 1, 1, 'Make sure Dane Pherigo has his LLC', False, '2020-02-11
+04:05:06');
 
 INSERT INTO "notes"
     ("clients_id", "users_id", "note", "timeStamp")
 VALUES
-    (1, 1, 'Keep up the good work', 2020-02-11
-04:05:06);
+    (1, 1, 'Keep up the good work', '2020-02-11
+04:05:06');
 
 INSERT INTO "events"
     ("eventDate", "startTime", "endTime", "endEventDate", "eventTitle", "notes", "location")
 VALUES
-    ( 2020-02-10, 2020-02-10
-016:00:00, 2020-02-10 018:00:00, 2020-02-10, 'Back To Business', 'Get ready to change lives', 'Prime Digital Academy');
+    ( 2020-02-10, '2020-02-10
+016:00:00', '2020-02-10 018:00:00', 2020-02-10, 'Back To Business', 'Get ready to change lives', 'Prime Digital Academy');
 
 INSERT INTO "survey"
     ("clients_id", "businessStarted", "revenue", "date")
