00ef63e62667c75ae8f84b836152b06e885c9b90
Josh Ganote <j.ganote0825@gmail.com>
2020-02-12 15:28:32 -0600

    added props to fullcalendar

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,18 +1,28 @@
+// React
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import EventModal from '../Calendar/EventModal';
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
+// Component
+import EventModal from '../Calendar/EventModal';
 
 // Material UI
 import Container from '@material-ui/core/Container';
 
+// CSS
+import '../Calendar/Calendar.css'
 
 
 class Calendar extends Component {
+  calendarComponentRef = React.createRef();
+
+  handleDateClick = (event) => {
+    
+  }
 
   render() {
     return (
@@ -21,7 +31,18 @@ class Calendar extends Component {
           <EventModal />
           <div>
             <Container maxWidth="md" className="calendar-container">
-              <FullCalendar defaultView="dayGridMonth" plugins={[dayGridPlugin]} />
+              <FullCalendar 
+                defaultView="dayGridMonth" 
+                header={{
+                  left: "prev, next, today",
+                  center: "title",
+                  right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek"
+                }}
+                ref={this.calendarComponentRef}
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                //events={} grab this off the reducer
+                dateClick={this.handleDateClick}
+              />
             </Container>
           </div>
         </div>
@@ -32,19 +53,3 @@ class Calendar extends Component {
 }
 
 export default connect(mapStoreToProps)(Calendar);
-=======
-
-class Calendar extends Component {
-    render() {
-        return(
-            <div>                                   
-                <p>
-                    Calendar Page!
-                </p>                                                  
-            </div>
-        )
-    }
-}
-
-export default Calendar;
->>>>>>> develop
