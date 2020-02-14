ac9bf5f6a1e4940b63c812600717e85305db35cb
Travis Johnston <travisjohnston4588@icloud.com>
2020-02-14 10:36:28 -0600

    resolved route conflicts

--- server/routes/competitions.router.ts
+++ server/routes/competitions.router.ts
@@ -6,20 +6,20 @@ import rejectUnauthenticated from '../modules/authentication-middleware';
 const router: express.Router = express.Router();
 
 //Route to create a list of competitions with joint clients to competitions
-router.get('/', (req: Request, res: Response): void => {
-  const queryText: string = `SELECT * FROM "clients" 
+router.get('/', rejectUnauthenticated, (req: Request, res: Response): void => {
+	const queryText: string = `SELECT * FROM "clients" 
     JOIN "clients_competitions" ON "clients_competitions".clients_id = "clients".id
     JOIN "competitions" ON "competitions".id = "clients_competitions".competitions_id
     ORDER BY "competitions"."dateOf" ASC LIMIT 10;`;
-  pool
-    .query(queryText)
-    .then(response => {
-      res.send(response.rows);
-    })
-    .catch(err => {
-      console.log(`Error retrieving client list ${err}`);
-      res.sendStatus(500);
-    });
+	pool
+		.query(queryText)
+		.then(response => {
+			res.send(response.rows);
+		})
+		.catch(err => {
+			console.log(`Error retrieving client list ${err}`);
+			res.sendStatus(500);
+		});
 });
 
 export default router;
