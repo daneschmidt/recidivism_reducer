f6e33beafd7c6a1a528a5e05148a85858f6137b6
Josh Ganote <j.ganote0825@gmail.com>
2020-02-17 09:16:05 -0600

    added user-credentials to root

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -7,6 +7,7 @@ import getClientsSaga from './client.saga';
 import getTasksSaga from './getTasksSaga';
 import getRecentCompsSaga from './competitions.saga';
 import calendar from './calendar.saga';
+import userCredentials from './user.credentials.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -25,5 +26,6 @@ export default function* rootSaga() {
 		getTasksSaga(),
 		getRecentCompsSaga(),
 		calendar(),
+		userCredentials(),
 	]);
 }
