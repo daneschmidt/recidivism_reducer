feb2f2a42ab8a1907e79b3274a791521b1e00730
Allen Lucke <allenlucke@gmail.com>
2020-02-18 11:58:07 -0600

    fixed issue with chchboxes not updating on byclients view

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -14,6 +14,7 @@ import getClientTasksSaga from './getClientTasks.saga';
 import getClientsSaga from './client.saga';
 import putTaskSaga from './putTask.saga';
 import postTaskSaga from './postTask.saga';
+import putClientTaskSaga from './putClientTask.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -39,5 +40,6 @@ export default function* rootSaga() {
     putTaskSaga(),
     postTaskSaga(),
     getProfileSaga(),
+    putClientTaskSaga(),
   ]);
 }
---
+++ src/redux/sagas/putClientTask.saga.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+// Put call for tasks
+function* putClientTask(action) {
+    console.log(`In put client task`);
+    const id= action.payload.id;
+    const clients_id= action.payload.clients_id
+    console.log(clients_id)
+    try {
+        const response = yield axios({
+            method: 'PUT',
+            url: '/api/tasks/put',
+            data: action.payload
+        });
+        yield put({
+            type: 'GET_TASKS_BY_CLIENT',
+            payload: {
+                sortBy: 'byClients',
+                clients_id,
+                trueOrFalse: 'False',
+                }
+            });
+    } catch(err) {
+        console.log('error putting task', err);
+    }
+}
+function* putClientTaskSaga() {
+    yield takeLatest('MARK_CLIENT_TASK', putClientTask);
+}
+
+export default putClientTaskSaga;
