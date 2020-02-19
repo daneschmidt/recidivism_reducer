909cced65f0176bccd278031fc6dee53f7ba8823
Allen Lucke <allenlucke@gmail.com>
2020-02-19 14:20:08 -0600

    added add user modal and saga

---
+++ src/redux/sagas/postUser.saga.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+// Post call to add new user
+function* postUser(action) {
+    console.log(`In postUserSaga`);
+    try {
+        const response = yield axios({
+            method: 'POST',
+            url: '/api/user/add',
+            data: action.payload
+        });
+        //
+        yield put({
+            type: 'GET_USER_CREDENTIALS',
+            payload: response.data
+        });
+    } catch(err) {
+        console.log('error adding user', err);
+    }
+}
+function* postUserSaga() {
+    yield takeLatest('POST_USER', postUser);
+}
+
+export default postUserSaga;
