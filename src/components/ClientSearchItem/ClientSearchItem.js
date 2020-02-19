ba63d1012654fb28400b4132ec271cebfaf2f354
Lucas Pherigo <lucas.pherigo@gmail.com>
2020-02-19 13:52:02 -0600

    components made for client result list

---
+++ src/components/ClientSearchItem/ClientSearchItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import mapStoreToProps from '../../redux/mapStoreToProps';
+
+class ClientSearchItem extends Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+
+export default connect(mapStoreToProps)(ClientSearchItem);
