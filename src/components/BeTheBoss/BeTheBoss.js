6f9b4e31ac85f88723de1f9b678e6b41c6357af3
Dane Schmidt <daneschmidt@gmail.com>
2020-02-21 14:48:31 -0600

    commit before pull from dev

--- src/components/BeTheBoss/BeTheBoss.js
+++ src/components/BeTheBoss/BeTheBoss.js
@@ -4,146 +4,36 @@ import { connect } from "react-redux";
 import mapStoreToProps from "../../redux/mapStoreToProps";
 
 
-// import Link from '@material-ui/core/Link';
-// // import { makeStyles } from '@material-ui/core/styles';
-// import Table from '@material-ui/core/Table';
-// import TableBody from '@material-ui/core/TableBody';
-// import TableCell from '@material-ui/core/TableCell';
-// import TableHead from '@material-ui/core/TableHead';
-// import TableRow from '@material-ui/core/TableRow';
+
 import GridItem from "../Grid/GridItem.js";
 import GridContainer from "../Grid/GridContainer.js";
 import Card from "../Card/Card.js";
-// import CardHeader from "../Card/CardHeader.js";
-// import CardBody from "../Card/CardBody.js";
-// import CardFooter from "../Card/CardFooter.js";
+
 
 import Paper from '@material-ui/core/Paper';
 
 import Button from '@material-ui/core/Button';
 
 import { withStyles, createStyles, Theme } from "@material-ui/core/styles";
+import TextField from '@material-ui/core/TextField';
 // import { Grid } from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
 
 import './BeTheBoss.css';
 
-const styles = () =>
-	createStyles({
-		// 	cardHeader: {
-		// 	padding: "0.75rem 1.25rem",
-		// 	marginBottom: "0",
-		// 	borderBottom: "none",
-		// 	background: "transparent",
-		// 	zIndex: "3 !important",
-		// 	"&$cardHeaderPlain,&$cardHeaderIcon,&$cardHeaderStats,&$warningCardHeader,&$successCardHeader,&$dangerCardHeader,&$infoCardHeader,&$primaryCardHeader,&$roseCardHeader": {
-		// 		margin: "0 15px",
-		// 		padding: "0",
-		// 		position: "relative",
-		// 		color: whiteColor
-		// 	},
-		// 	"&:first-child": {
-		// 		borderRadius: "calc(.25rem - 1px) calc(.25rem - 1px) 0 0"
-		// 	},
-		// 	"&$warningCardHeader,&$successCardHeader,&$dangerCardHeader,&$infoCardHeader,&$primaryCardHeader,&$roseCardHeader": {
-		// 		"&:not($cardHeaderIcon)": {
-		// 			borderRadius: "3px",
-		// 			marginTop: "-20px",
-		// 			padding: "15px"
-		// 		}
-		// 	},
-		// 	"&$cardHeaderStats svg": {
-		// 		fontSize: "36px",
-		// 		lineHeight: "56px",
-		// 		textAlign: "center",
-		// 		width: "36px",
-		// 		height: "36px",
-		// 		margin: "10px 10px 4px"
-		// 	},
-		// 	"&$cardHeaderStats i,&$cardHeaderStats .material-icons": {
-		// 		fontSize: "36px",
-		// 		lineHeight: "56px",
-		// 		width: "56px",
-		// 		height: "56px",
-		// 		textAlign: "center",
-		// 		overflow: "unset",
-		// 		marginBottom: "1px"
-		// 	},
-		// 	"&$cardHeaderStats$cardHeaderIcon": {
-		// 		textAlign: "right"
-		// 	}
-		// },
-		// cardHeaderPlain: {
-		// 	marginLeft: "0px !important",
-		// 	marginRight: "0px !important"
-		// },
-		// cardHeaderStats: {
-		// 	"& $cardHeaderIcon": {
-		// 		textAlign: "right"
-		// 	},
-		// 	"& h1,& h2,& h3,& h4,& h5,& h6": {
-		// 		margin: "0 !important"
-		// 	}
-		// },
-		// cardHeaderIcon: {
-		// 	"&$warningCardHeader,&$successCardHeader,&$dangerCardHeader,&$infoCardHeader,&$primaryCardHeader,&$roseCardHeader": {
-		// 		background: "transparent",
-		// 		boxShadow: "none"
-		// 	},
-		// 	"& i,& .material-icons": {
-		// 		width: "33px",
-		// 		height: "33px",
-		// 		textAlign: "center",
-		// 		lineHeight: "33px"
-		// 	},
-		// 	"& svg": {
-		// 		width: "24px",
-		// 		height: "24px",
-		// 		textAlign: "center",
-		// 		lineHeight: "33px",
-		// 		margin: "5px 4px 0px"
-		// 	}
-		// },
-		// warningCardHeader: {
-		// 	color: whiteColor,
-		// 	"&:not($cardHeaderIcon)": {
-		// 		...warningCardHeader
-		// 	}
-		// },
-		// successCardHeader: {
-		// 	color: whiteColor,
-		// 	"&:not($cardHeaderIcon)": {
-		// 		...successCardHeader
-		// 	}
-		// },
-		// dangerCardHeader: {
-		// 	color: whiteColor,
-		// 	"&:not($cardHeaderIcon)": {
-		// 		...dangerCardHeader
-		// 	}
-		// },
-		// infoCardHeader: {
-		// 	color: whiteColor,
-		// 	"&:not($cardHeaderIcon)": {
-		// 		...infoCardHeader
-		// 	}
-		// },
-		// primaryCardHeader: {
-		// 	color: whiteColor,
-		// 	"&:not($cardHeaderIcon)": {
-		// 		...primaryCardHeader
-		// 	}
-		// },
-		// roseCardHeader: {
-		// 	color: whiteColor,
-		// 	"&:not($cardHeaderIcon)": {
-		// 		...roseCardHeader
-		// 	}
-		// }
-
-	})
+const styles = (theme) => createStyles({
+	root: {
+		'& > *': {
+			margin: theme.spacing(1),
+			width: 200,
+		},
+	},
+});
 
 
 class BeTheBoss extends Component {
+
 	state = {
 		firstName: '',
 		lastName: '',
@@ -203,7 +93,9 @@ class BeTheBoss extends Component {
 					<Card>
 						<Paper className="paperPanel" elevation={5}>
 							<h1 className='beTheBoss'>Be The Boss</h1>
-							<input
+							<TextField
+								id="standard-basic"
+								label="Standard"
 								type="text"
 								className="inputs"
 								placeholder="First Name"
@@ -212,7 +104,9 @@ class BeTheBoss extends Component {
 								required
 							/>
 							<br />
-							<input
+							<TextField
+								id="standard-basic"
+								label="Standard"
 								type="text"
 								className="inputs"
 								placeholder="Last Name"
@@ -220,7 +114,9 @@ class BeTheBoss extends Component {
 								onChange={event => this.changeField(event, "lastName")}
 							/>
 							<br />
-							<input
+							<TextField
+								id="standard-basic"
+								label="Standard"
 								type="text"
 								className="inputs"
 								placeholder="Gender"
@@ -228,7 +124,9 @@ class BeTheBoss extends Component {
 								onChange={event => this.changeField(event, "gender")}
 							/>
 							<br />
-							<input
+							<TextField
+								id="standard-basic"
+								label="Standard"
 								type="number"
 								className="inputs"
 								placeholder="Phone Number"
@@ -236,7 +134,9 @@ class BeTheBoss extends Component {
 								onChange={event => this.changeField(event, "phoneNumber")}
 							/>
 							<br />
-							<input
+							<TextField
+								id="standard-basic"
+								label="Standard"
 								type="text"
 								className="inputs"
 								placeholder="Email"
@@ -244,8 +144,9 @@ class BeTheBoss extends Component {
 								onChange={event => this.changeField(event, "email")}
 							/>
 							<br />
-							<button variant="contained" color="primary" onClick={this.addClientInfo}>Submit</button>
+
 						</Paper>
+						<button variant="contained" color="primary" onClick={this.addClientInfo}>Submit</button>
 					</Card>
 				</GridItem>
 			</GridContainer>
@@ -253,5 +154,5 @@ class BeTheBoss extends Component {
 	}
 }
 
-export default connect(mapStoreToProps)(BeTheBoss);
+export default connect(mapStoreToProps)(withStyles(styles)(BeTheBoss));
 // export default connect(mapStoreToProps)(withStyles(styles)(BtnImgBaseClass));
